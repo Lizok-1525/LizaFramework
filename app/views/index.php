51247a94e19dae6fba5091068a65a08f8054e13a
Lizok-1525 <liza@ma-no.org>
2025-04-01 16:02:10 +0200

    update app/views/index.php

--- app/views/index.php
+++ app/views/index.php
@@ -10,7 +10,7 @@
       <p>Apasionada de la comunicación y la narración de historias, con un fuerte deseo de convertirme en periodista. Mi curiosidad innata y mi compromiso con la verdad me impulsan a investigar y explorar temas de relevancia social. A través de mi formación académica y experiencias prácticas, he desarrollado habilidades en redacción, investigación y análisis crítico, que me permiten abordar diversas temáticas con profundidad y objetividad. </p>
       <p>Además, cuento con conocimientos en programación web, lo que me permite crear y gestionar contenido digital de manera efectiva. Estoy emocionada por la oportunidad de contribuir al mundo del periodismo, informando y conectando a las personas con las historias que importan.</p>
       <?php echo "Buenos dias!!";
-      echo "Buenos diaaaaas!!"; ?>
+      echo "    Buenos diaaaaas!!"; ?>
     </div>
   </div>
   <?php include(BASE_PATH . "/inc/navegacion.inc.php"); ?>
