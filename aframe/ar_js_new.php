0ff6155bb7bfe9c124a27897bde843d7033c1610
Lizok-1525 <liza@ma-no.org>
2025-04-08 12:18:38 +0200

    chore: delete ar_js_new.html

--- aframe/ar_js_new.php
+++ aframe/ar_js_new.php
@@ -5,7 +5,6 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>GeoAR.js demo</title>
-
     <script src="https://aframe.io/releases/1.6.0/aframe.min.js"></script>
     <script type='text/javascript'
         src='https://raw.githack.com/AR-js-org/AR.js/3.4.5/three.js/build/ar-threex-location-only.js'></script>
@@ -20,8 +19,6 @@
     <a-scene vr-mode-ui="enabled: false" embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'
         renderer='antialias: true; alpha: true'>
-
-
         <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15"
             gps-entity-place="longitude: 2.6608208; latitude: 39.5734182;" animation-mixer />
         <a-camera gps-camera rotation-reader></a-camera>
