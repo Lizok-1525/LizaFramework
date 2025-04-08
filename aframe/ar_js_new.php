6ce222d17b97865046ced3c39b4d6e5299c1bfa3
Lizok-1525 <liza@ma-no.org>
2025-04-08 15:44:11 +0200

    update ar_js_new.php

--- aframe/ar_js_new.php
+++ aframe/ar_js_new.php
@@ -53,11 +53,22 @@
     <button id="getLocationBtn">Show my location</button><br />
     <p id="location"></p>
 
-    <a-scene vr-mode-ui="enabled: false" embedded
-        arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'
-        renderer='antialias: true; alpha: true'>
-        <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15"
-            gps-entity-place="longitude: 2.6577131; latitude: 39.5727872;" animation-mixer />
+    <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth:1280; displayHeight:960; debugUIEnabled: false;"
+        renderer="antialias: true; alpha: true">
+
+        <!-- MODELO EN POSICIÓN GPS FIJA -->
+        <a-entity
+            gltf-model="./assets/magnemite/scene.gltf"
+            scale="0.15 0.15 0.15"
+            rotation="0 180 0"
+            gps-entity-place="longitude: 2.6577131; latitude: 39.5727872;"
+            animation-mixer>
+        </a-entity>
+
+        <!-- CÁMARA GPS -->
         <a-camera gps-camera rotation-reader></a-camera>
     </a-scene>
 
