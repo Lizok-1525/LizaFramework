35b4d2fd01760003fc04b31264415394d8a9b4ca
Lizok-1525 <liza@ma-no.org>
2025-04-09 09:30:39 +0200

    update ar_js_new.php

--- aframe/ar_js_new.php
+++ aframe/ar_js_new.php
@@ -57,11 +57,7 @@
         vr-mode-ui="enabled: false"
         embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
-        <a-entity gltf-model="./assets/magnemite/scene.gltf"
-            position="0 0 -3"
-            scale="0.15 0.15 0.15"
-            animation-mixer
-            look-at="[gps-camera]"></a-entity>
+        <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="longitude: 2.6598294; latitude: 39.573038;" animation-mixer />
         <a-camera gps-camera rotation-reader></a-camera>
     </a-scene>
 
