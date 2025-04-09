1e687545fdfbc4fb0f8b2e3e3542c167111f789d
Lizok-1525 <liza@ma-no.org>
2025-04-09 09:44:44 +0200

    update ar_js_new.php

--- aframe/ar_js_new.php
+++ aframe/ar_js_new.php
@@ -57,12 +57,50 @@
         vr-mode-ui="enabled: false"
         embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
-        <a-entity gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" scale="0.15 0.15 0.15" gps-entity-place="latitude: 39.573038; longitude: 2.6598294;" animation-mixer />
-        <a-camera gps-camera rotation-reader></a-camera>
+        <a-camera gps-camera rotation-reader>
+        </a-camera>
     </a-scene>
 
 
+    <script>
+        window.onload = () => {
+            let places = staticLoadPlaces();
+            renderPlaces(places);
+        };
 
+        function staticLoadPlaces() {
+            return [{
+                name: 'Magnemite',
+                location: {
+                    lat: 39.57307,
+
+                    lng: 2.65917,
+                }
+            }, ];
+        }
+
+        function renderPlaces(places) {
+            let scene = document.querySelector('a-scene');
+
+            places.forEach((place) => {
+                let latitude = place.location.lat;
+                let longitude = place.location.lng;
+
+                let model = document.createElement('a-entity');
+                model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+                model.setAttribute('rotation', '0 180 0');
+                model.setAttribute('animation-mixer', '');
+                model.setAttribute('scale', '0.5 0.5 0.5');
+
+                model.addEventListener('loaded', () => {
+                    window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                });
+
+                scene.appendChild(model);
+            });
+        }
+    </script>
     <script>
         document.getElementById('getLocationBtn').addEventListener('click', function() {
             if (navigator.geolocation) {
