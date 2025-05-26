07ac60e5ce87c03be572157cf353d184f369a4b4
Lizok-1525 <liza@ma-no.org>
2025-05-26 14:53:27 +0200

    update keypad_servo.ino

--- ARDUINO/keypad_servo.ino
+++ ARDUINO/keypad_servo.ino
@@ -17,13 +17,23 @@ byte colPins[COLS] = {5, 4, 3, 2}; // Conecta a las columnas
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 Servo servo;
 
-String password = "0911";
+const int ledVerde = 11;  // Pin LED verde
+const int ledRojo = 12;   // Pin LED rojo
+
+String password = "A321";
 String input = "";
 
 void setup() {
   Serial.begin(9600);
   servo.attach(10); // Conecta el servo al pin 10
   servo.write(0);   // Posición cerrada
+  
+  pinMode(ledVerde, OUTPUT);
+  pinMode(ledRojo, OUTPUT);
+  
+  digitalWrite(ledVerde, LOW);
+  digitalWrite(ledRojo, LOW);
+  
   Serial.println("Introduce la clave:");
 }
 
@@ -36,16 +46,27 @@ void loop() {
     if (key == '#') {
       if (input == password) {
         Serial.println("\n¡Clave correcta!");
+        digitalWrite(ledVerde, HIGH);  // Enciende LED verde
+        digitalWrite(ledRojo, LOW);
+        
         servo.write(120); // Abre la cerradura
         delay(2000);
         servo.write(0);  // Vuelve a cerrar
+        
+        digitalWrite(ledVerde, LOW);   // Apaga LED verde
       } else {
         Serial.println("\nClave incorrecta");
+        digitalWrite(ledRojo, HIGH);   // Enciende LED rojo
+        digitalWrite(ledVerde, LOW);
+        delay(2000);
+        digitalWrite(ledRojo, LOW);    // Apaga LED rojo
       }
       input = ""; // Reinicia la entrada
     } else if (key == '*') {
       input = ""; // Borra entrada
       Serial.println("\nEntrada reiniciada");
+      digitalWrite(ledRojo, LOW);
+      digitalWrite(ledVerde, LOW);
     } else {
       input += key; // Agrega dígito
     }
