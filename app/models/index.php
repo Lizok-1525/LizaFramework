00fa6fb385a39ac2de16a8e8e10be16c76366308
Lizok-1525 <liza@ma-no.org>
2025-05-29 17:22:28 +0200

    update .htaccess, app/models/index.php and app/views/index.php

--- app/models/index.php
+++ app/models/index.php
@@ -7,4 +7,3 @@ $head_description = "Curriculum de Yelyzaveta Krasnolutska, con experiencias y c
 $canonical_name = "https://liza.ma-no.es/";
 ?>
 <script src="artyom.window.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
