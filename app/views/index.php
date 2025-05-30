054ec2b4d5104579838d136919e7a78601bf1b17
Lizok-1525 <liza@ma-no.org>
2025-05-30 11:50:32 +0200

    update app/views/index.php

--- app/views/index.php
+++ app/views/index.php
@@ -5,10 +5,10 @@
       </div>
     </div>
     <div class="col-sm-8 p-5 intro bg-light text-dark">
-      <h2>Yelyzaveta Krasnolutska</h2>
-      <h3><i>Periodista</i></h3>
-      <p>Apasionada de la comunicación y la narración de historias, con un fuerte deseo de convertirme en periodista. Mi curiosidad innata y mi compromiso con la verdad me impulsan a investigar y explorar temas de relevancia social. A través de mi formación académica y experiencias prácticas, he desarrollado habilidades en redacción, investigación y análisis crítico, que me permiten abordar diversas temáticas con profundidad y objetividad. </p>
-      <p>Además, cuento con conocimientos en programación web, lo que me permite crear y gestionar contenido digital de manera efectiva. Estoy emocionada por la oportunidad de contribuir al mundo del periodismo, informando y conectando a las personas con las historias que importan.</p>
+      <h2 class="p-2">Yelyzaveta Krasnolutska</h2>
+      <h3 class="p-2"><i>Periodista</i></h3>
+      <p class="p-2">Apasionada de la comunicación y la narración de historias, con un fuerte deseo de convertirme en periodista. Mi curiosidad innata y mi compromiso con la verdad me impulsan a investigar y explorar temas de relevancia social. A través de mi formación académica y experiencias prácticas, he desarrollado habilidades en redacción, investigación y análisis crítico, que me permiten abordar diversas temáticas con profundidad y objetividad. </p>
+      <p class="p-2">Además, cuento con conocimientos en programación web, lo que me permite crear y gestionar contenido digital de manera efectiva. Estoy emocionada por la oportunidad de contribuir al mundo del periodismo, informando y conectando a las personas con las historias que importan.</p>
       <?php
       ?>
       <div class="p-2">
