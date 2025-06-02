82b8558699b8e1ec6ff2538bc0b47dcd2f75fc5a
Lizok-1525 <liza@ma-no.org>
2025-06-02 16:45:02 +0200

    update testeo.php

--- app/views/testeo.php
+++ app/views/testeo.php
@@ -11,7 +11,7 @@
     }*/
 
     .page-link.active {
-        background-color: #ff6600;
+        background-color: rgb(243, 255, 190);
         /* Cambia este color al que quieras */
         border-color: #ff6600;
         /* Opcional, para que el borde combine */
