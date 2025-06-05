1bc6fbf8caa8180776d166402fb0875498096884
Lizok-1525 <liza@ma-no.org>
2025-06-05 11:59:37 +0200

    update test.php

--- aframe/test.php
+++ aframe/test.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-  <title>Pruebas</title>
+  <title>The birds</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://aframe.io/aframe/dist/aframe-master.min.js"></script>
